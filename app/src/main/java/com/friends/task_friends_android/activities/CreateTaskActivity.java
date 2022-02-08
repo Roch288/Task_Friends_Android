30feefcedf935c1689db63aed071b224d6b0c2e9
nishuroka11 <c0837286@mylambton.ca>
2022-02-07 22:25:42 -0500

    capture date and time

--- app/src/main/java/com/friends/task_friends_android/activities/CreateTaskActivity.java
+++ app/src/main/java/com/friends/task_friends_android/activities/CreateTaskActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.task_friends_android.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class CreateTaskActivity extends AppCompatActivity {
+
+    private EditText inputTaskTitle, inputTaskCategory, inputTaskDesc;
+    private TextView textCreateDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,16 @@ public class CreateTaskActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        inputTaskTitle = findViewById(R.id.inputTaskTitle);
+        inputTaskCategory = findViewById(R.id.inputTaskCategory);
+        inputTaskDesc = findViewById(R.id.inputTaskDesc);
+        textCreateDateTime = findViewById(R.id.textCreateDateTime);
+
+
+        textCreateDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                        .format(new Date())
+        );
     }
 }
